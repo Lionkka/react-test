d6ec3c26718d0ff772587f26589711ced507a63f
BigBrosh <maksuhin@gmail.com>
2019-01-19 16:50:53 +0200

    - Navigation turned into purecomponent

--- src/components/Router/Navigation/Navigation.js
+++ src/components/Router/Navigation/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => (
-  <nav>
-    <Link to='/'>
-        Home
-    </Link>
+export default class extends React.PureComponent {
+  render() {
+    console.log('render pure navigation');
 
-    <Link to='/about'>
-        About
-    </Link>
-  </nav>
-);
+    return (
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+    );
+  }
+}
