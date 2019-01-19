598ad496b625c79d1e7dd2202a55416e80a830fa
BigBrosh <maksuhin@gmail.com>
2019-01-19 09:16:19 +0200

    changed data structure to handle multiple examples from component folder.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,7 @@
-import React, { Component } from 'react'
+import React from "react";
 import './App.css'
-import { Item, ClassItem } from './components/Item'
-import Field from './components/Field'
+import Todos from './components/Todos';
 
-class App extends Component {
-
-  constructor (props) {
-    console.log('constructor')
-    super(props)
-    this.state = {
-      list: null,
-      hideField: false
-    }
-  }
-  componentDidMount(){
-    console.log('componentDidMount')
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(res=> res.json())
-      .then((list)=> {
-        console.log('data', list.slice(0,1))
-        this.setState({list}, ()=> console.log(this.state.list))
-      })
-
-    setTimeout(()=>{
-      this.setState({hideField: true})
-    }, 1000)
-  }
-
-  componentDidUpdate(){
-    console.log('componentDidUpdate')
-  }
-
-  render () {
-
-    console.log('render')
-
-    const {list, hideField} = this.state
-    return (
-      <div className="App">
-        {!hideField && <Field/>}
-        <ul>
-          {list && list.map((todo, i) =>
-            <Item
-              key={i}
-              data={todo}
-            />)}
-        </ul>
-      </div>
-    )
-  }
-}
-
-export default App
+export default () => (
+    <Todos />
+);
---
+++ src/components/Todos/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import Field from "./Field";
+import Item from "./Item";
+
+class App extends Component {
+
+    constructor (props) {
+        console.log('constructor')
+        super(props)
+        this.state = {
+            list: null,
+            hideField: false
+        }
+    }
+    componentDidMount(){
+        console.log('componentDidMount')
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(res=> res.json())
+            .then((list)=> {
+                console.log('data', list.slice(0,1))
+                this.setState({list}, ()=> console.log(this.state.list))
+            })
+
+        setTimeout(()=>{
+            this.setState({hideField: true})
+        }, 1000)
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate')
+    }
+
+    render () {
+
+        console.log('render')
+
+        const {list, hideField} = this.state
+        return (
+            <div className="App">
+                {!hideField && <Field/>}
+                <ul>
+                    {list && list.map((todo, i) =>
+                        <Item
+                            key={i}
+                            data={todo}
+                        />)}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default App
