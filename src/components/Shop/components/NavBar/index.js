f3344a84e119d7dc358d6206f0ffb1ece9e4838b
BigBrosh <maksuhin@gmail.com>
2019-03-02 14:34:15 +0200

    - implemented compose.

--- src/components/Shop/components/NavBar/index.js
+++ src/components/Shop/components/NavBar/index.js
@@ -4,7 +4,14 @@ import styles from './styles';
 import { connect } from 'react-redux';
 import toggleSidebar from '../../actions/sidebar/toggleSidebar';
 
-export default connect(
-  null,
-  { toggleSidebar }
-)(withStyles(styles)(NavBar));
+import { compose } from 'recompose';
+
+const enhance = compose(
+  connect(
+    null,
+    { toggleSidebar }
+  ),
+  withStyles(styles)
+);
+
+export default enhance(NavBar);
--- src/components/Shop/components/SideBar/index.js
+++ src/components/Shop/components/SideBar/index.js
@@ -1,12 +1,18 @@
 import SideBar from './SideBar';
+import { compose } from 'recompose';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 import connect from 'react-redux/es/connect/connect';
 import toggleSidebar from '../../actions/sidebar/toggleSidebar';
 
-export default connect(
-  (state) => ({
-    isOpen: state.sidebar.isOpen
-  }),
-  { toggleSidebar }
-)(withStyles(styles)(SideBar));
+const enhance = compose(
+  connect(
+    (state) => ({
+      isOpen: state.sidebar.isOpen
+    }),
+    { toggleSidebar }
+  ),
+  withStyles(styles)
+);
+
+export default enhance(SideBar);
